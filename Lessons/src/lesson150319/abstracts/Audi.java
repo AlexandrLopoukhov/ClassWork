4e9430ba6c92f17e601d5891053d96f6cb0a731f
AlexandrLopoukhov <tigraride@gmail.com>
2015-03-20 00:26:33 +0300

    Update Audi.java

--- Lessons/src/lesson150319/abstracts/Audi.java
+++ Lessons/src/lesson150319/abstracts/Audi.java
@@ -3,7 +3,7 @@ package lesson150319.abstracts;
 public class Audi extends Car {
 
 	@Override
-	public void moveTo(final int x, final int y) {
+	public void moveTo(int x, int y) {
 		turnOn();
 
 		driver();
