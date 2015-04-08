3e340ddf74c0eede5f069cc755dd2f4e88f51ed5
Alexandr Lopoukhov <tigraride@gmail.com>
2015-04-08 11:11:44 +0300

    merged conflict

--- Lessons/src/lesson150402/exceptions/Checked.java
+++ Lessons/src/lesson150402/exceptions/Checked.java
@@ -3,27 +3,15 @@ package lesson150402.exceptions;
 public class Checked {
 
 	public static void main(final String[] args) {
-		// throw new Throwable(); //Провряемое
-		// throw new RuntimeException(); непроверяемое
-		// throw new Error(); непроверяемое
-		// execption проверяремое
-	}
-=======
-	
-	public static void main(final String[] args) {
-		
-		
-//		try {
-//			throw new Throwable();
-//		} catch (Throwable e) {
-//			e.printStackTrace();
-//		}
-//		throw new RuntimeException();
-//		throw new Error();
-		
-		
-	}
-	
 
->>>>>>> refs/remotes/zaal/master
+		// try {
+		// throw new Throwable();
+		// } catch (Throwable e) {
+		// e.printStackTrace();
+		// }
+		// throw new RuntimeException();
+		// throw new Error();
+
+	}
+
 }
--- Lessons/src/lesson150402/exceptions/DivisionByZero.java
+++ Lessons/src/lesson150402/exceptions/DivisionByZero.java
@@ -1,33 +1,16 @@
 package lesson150402.exceptions;
 
 public class DivisionByZero {
-	public static void main(final String[] args) {
-		int result;
-		try {
-			result = div(10, 0);
-		} catch (DividerByZero e) {
-			result = Integer.MAX_VALUE;
-		}
-		System.out.println(result);
-	}
-
-	public static int div(final int i, final int j) throws DividerByZero {
-		if (j == 0) {
-			throw new DividerByZero();
-		}
-		return i / j;
-
-=======
 
 	public static void main(final String[] args) throws DividerIsZero {
 
 		int result = 0;
-//		try {
-			result = div(100, 0);
-//		} catch (DividerIsZero e) {
-//			result = Integer.MAX_VALUE;
-//			e.printStackTrace();
-//		}
+		// try {
+		result = div(100, 0);
+		// } catch (DividerIsZero e) {
+		// result = Integer.MAX_VALUE;
+		// e.printStackTrace();
+		// }
 
 		System.out.println(result);
 
@@ -38,8 +21,8 @@ public class DivisionByZero {
 		if (j == 0) {
 			throw new DividerIsZero();
 		}
-		
+
 		return i / j;
->>>>>>> refs/remotes/zaal/master
+
 	}
 }
